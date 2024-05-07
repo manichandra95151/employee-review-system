3795ecb38b2fdcfc753900f0a6ea323869a168e1
Mani Chandra <82253140+manichandra95151@users.noreply.github.com>
2024-05-07 17:20:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const express = require('express');
 const cookieParser = require('cookie-parser'); // parse cookie header and populate req.cookies
 const bodyParser = require('body-parser'); // parses incoming request bodies (req.body)
 const app = express();
-const { PORT, MONGODB_URL, SESSION_SECRET_KEY } = process.env;
 const expressLayouts = require('express-ejs-layouts');
 
 // used for session cookie
@@ -33,12 +32,19 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(
   session({
     name: 'employee-review-system',
-    secret: SESSION_SECRET_KEY,
+    secret: process.env.SESSION_SECRET_KEY,
     saveUninitialized: false,
     resave: false,
     cookie: {
       maxAge: 1000 * 60 * 100,
-    }
+    },
+    store: MongoStore.create({
+      mongoUrl: process.env.MONGODB_URL,
+      autoRemove: 'disabled',
+    }),
+    function(err) {
+      console.log(err || 'connect-mongodb setup ok');
+    },
   })
 );
 
@@ -54,9 +60,9 @@ app.use(customMware.setFlash);
 // use express router44
 app.use('/', require('./routes'));
 
-app.listen(PORT || 5000, (err) => {
+app.listen(process.env.PORT || 5000, (err) => {
   if (err) {
     console.log(`Error in running the server: ${err}`);
   }
-  console.log(`Server is running on port: ${PORT}`);
+  console.log(`Server is running on port: ${process.env.PORT}`);
 });
